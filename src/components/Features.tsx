d1b6a7b6fe5bace6083c7582096948379b2b6be5
GermanMorales <germanricardomorales99@gmail.com>
2024-10-03 21:54:08 -0500

    fix

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -23,7 +23,7 @@ const Features = () => {
 
         <div className="mt-10">
           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            {featuresList.map((feature) => (
+            {featuresList.slice(0, 2).map((feature) => (
               <a
                 href={feature.url}
                 key={feature.name}
